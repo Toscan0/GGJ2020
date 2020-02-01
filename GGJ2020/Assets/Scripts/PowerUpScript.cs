b3426c4394ef6d1ff316a778c93b379f45b3b3f0
Carolina <carolveloso97@gmail.com>
2020-02-01 01:40:17 +0000

    PowerUp WIP

---
+++ GGJ2020/Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+
+    public GameObject powerUp;
+    //private Transform spawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawn = transform;
+        //spawn.position.y = spawn.position.y + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn PowerUp
+        if(Input.GetMouseButton(0))
+        {
+            GameObject spawn = Instantiate(powerUp);
+            spawn.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "player") {
+            //Verify powerup
+            //Destroy powerup
+        }
+    }
+}
