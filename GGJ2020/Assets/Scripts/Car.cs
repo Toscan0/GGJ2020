cd42af429175d8b6a52866f7baab45da6144998d
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-02-01 01:13:29 +0000

    os carros movem

---
+++ GGJ2020/Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed; 
+    //public float minSpeed = 8f;
+    //public float maxSpeed = 12f;
+
+    //float speed = 1f;
+
+    void Start()
+    {
+        //speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forward = new Vector3(transform.right.x, transform.right.y, transform.right.z);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+
+}
---
+++ GGJ2020/Assets/Scripts/DestroyCar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCar : MonoBehaviour
+{
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "Car")
+        {
+            Destroy(obj.gameObject);
+        }
+    }
+}
