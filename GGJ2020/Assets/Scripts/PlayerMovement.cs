9f3b5138e2a35faea786e2a52a88d79cd60075bb
nunesfariasmar <mariana.farias@tecnico.ulisboa.pt>
2020-02-01 09:30:08 +0000

    Added the character assets to a new scene.

--- GGJ2020/Assets/Scripts/PlayerMovement.cs
+++ GGJ2020/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,25 @@ public class PlayerMovement : MonoBehaviour
 
     public int playerId = 1;
 
+    public Animator animator;
+    public float rotation_speed = 0.5f;
+    public static float MAX_SPEED = 1.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal" + (playerId > 1 ? "" + playerId : "")) / 50, 0, Input.GetAxis("Vertical" + (playerId > 1 ? "" + playerId : "")) / 50));        
+        var movement = new Vector3(Input.GetAxis("Horizontal" + (playerId > 1 ? "" + playerId : "")) / 50, 0,
+            Input.GetAxis("Vertical" + (playerId > 1 ? "" + playerId : "")) / 50);
+        transform.Translate(movement);
+        animator.SetBool("Running", movement.magnitude > 0);
     }
 
     public void OnCollisionEnter(Collision collision)
