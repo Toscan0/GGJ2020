1bd469574d60419865acdf9eaa7f5638f9cdc4cb
Denis Voicu <dennydenii@gmail.com>
2020-02-01 03:55:50 +0000

    assets dos carros e aparecimento aleatorio de carros nas garagens

---
+++ GGJ2020/Assets/Scripts/CarGarageSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarGarageSpawner : MonoBehaviour
+{
+
+    public GameObject[] garages;
+    public GameObject car;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnCars", 5, 5);
+    }
+
+    void SpawnCars()
+    {
+        GameObject garage = garages[Random.Range(0, 4)];
+        if (garage.name == "Cube" || garage.name == "Cube (1)" || garage.name == "Cube (2)")
+        {
+            car.transform.localScale = new Vector3(58f, 58f, 58f);
+            GameObject aux = Instantiate(car, garage.transform.position, Quaternion.AngleAxis(-90, new Vector3(0, 1, 0)));
+        }
+        else
+        {
+            car.transform.localScale = new Vector3(58f, 58f, 58f);
+            GameObject aux = Instantiate(car, garage.transform.position, Quaternion.AngleAxis(90, new Vector3(0, 1, 0)));
+        }
+    }
+}
